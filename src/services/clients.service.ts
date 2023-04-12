689565e2f1fd271ef8c89f142c50b7a4bf1a1629
Carlos Ferreira <carlos@especializati.com.br>
2023-04-12 17:38:24 -0300

    feat: list all clients

--- src/services/clients.service.ts
+++ src/services/clients.service.ts
@@ -1,11 +1,20 @@
+import axios from "axios";
 import { Request, Response } from "express"
 
 class ClientService {
   findAll = async (request: Request, response: Response): Promise<Response> => {
-    
+    const { data, status } = await axios.get(
+      process.env.SPEDY_URL_API + '/customers',
+      {
+        headers: {
+          Accept: 'application/json',
+          'X-Api-Key': process.env.SPEDY_TOKEN_API
+        },
+      },
+    )
 
-    return response.status(200).send({
-      ok: 'ok'
+    return response.status(status).send({
+      data: data
     })
   }
 }
