97e051b6bfb0ceacbf7f1ed53d3df96ca592f1eb
Carlos Ferreira <carlos@especializati.com.br>
2023-04-12 17:14:27 -0300

    feat: running server with express

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
+import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.listen(PORT, () => {
+  console.log(`server running in port  ${PORT}`)
+})
