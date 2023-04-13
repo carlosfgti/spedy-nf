7757eb604c7bddb96e0405ff01f4a0fb12629c29
Carlos Ferreira <carlos@especializati.com.br>
2023-04-13 10:40:15 -0300

    refactor: create method post

--- src/services/http/http.service.ts
+++ src/services/http/http.service.ts
@@ -1,16 +1,28 @@
 import axios from "axios"
 import { DefaultResponse } from "./DefaultResponse"
 
+const defaultHeader = {
+  headers: {
+    Accept: 'application/json',
+    'X-Api-Key': process.env.SPEDY_TOKEN_API
+  },
+}
+
 export class HttpService {
   static get = async (endpoint: string): Promise<DefaultResponse> => {
     const { data, status } = await axios.get(
       process.env.SPEDY_URL_API + endpoint,
-      {
-        headers: {
-          Accept: 'application/json',
-          'X-Api-Key': process.env.SPEDY_TOKEN_API
-        },
-      },
+      defaultHeader,
+    )
+
+    return { data, status }
+  }
+
+  static post = async (endpoint: string, body: object): Promise<DefaultResponse> => {
+    const { data, status } = await axios.post(
+      process.env.SPEDY_URL_API + endpoint,
+      body,
+      defaultHeader,
     )
 
     return { data, status }
